688fac0286d4c6a0fb99fb1bbab6cb237385b441
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-02-16 12:14:56 +0530

    fixed

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,28 @@
+const contentInfo = {
+  srcLink1: "../src/assests/contact-img.jpg",
+  srcLink2: "../src/assests/map-img.jpg",
+  srcLink3: "../src/assests/email-img.png",
+  srcLink4: "../src/assests/phone-img.png",
+  paraText1: "1. Location ( Kanpur )",
+  paraText2: "2. Email ( ahmadiklaq3@gmail.com)",
+  paraText3: "3. Phone ( +916307329321 )",
+};
+
+function addImageElement(contentDiv, srcLink) {
+  const contactImage = document.createElement("img");
+  contactImage.classList.add("menu-img");
+  contactImage.src = srcLink;
+  contactImage.alt = "Image of Menu";
+  contentDiv.appendChild(contactImage);
+}
+
+function addParaElement(contentDiv, paraText) {
+  const para = document.createElement("p");
+  para.classList.add("intro");
+  para.textContent = paraText;
+  contentDiv.appendChild(para);
+}
+
 export function loadContactPage() {
   const contentDiv = document.querySelector("#content");
   contentDiv.replaceChildren();
@@ -5,43 +30,11 @@ export function loadContactPage() {
   heading.textContent = "Contact :)";
   heading.classList.add("landing-title");
   contentDiv.appendChild(heading);
-
-  const topImage = document.createElement("img");
-  topImage.classList.add("menu-img");
-  topImage.src = "../src/assests/contact-img.jpg";
-  topImage.alt = "Image of Menu";
-  contentDiv.appendChild(topImage);
-
-  const para1 = document.createElement("p");
-  para1.classList.add("intro");
-  para1.textContent = "1. Location ( Kanpur )";
-  contentDiv.appendChild(para1);
-
-  const contactImage1 = document.createElement("img");
-  contactImage1.classList.add("menu-img");
-  contactImage1.src = "../src/assests/map-img.jpg";
-  contactImage1.alt = "Image of Menu";
-  contentDiv.appendChild(contactImage1);
-
-  const para2 = document.createElement("p");
-  para2.classList.add("intro");
-  para2.textContent = "2. Email ( ahmadiklaq3@gmail.com)";
-  contentDiv.appendChild(para2);
-
-  const contactImage2 = document.createElement("img");
-  contactImage2.classList.add("menu-img");
-  contactImage2.src = "../src/assests/email-img.png";
-  contactImage2.alt = "Image of Menu";
-  contentDiv.appendChild(contactImage2);
-
-  const para3 = document.createElement("p");
-  para3.classList.add("intro");
-  para3.textContent = "3. Phone ( +916307329321 )";
-  contentDiv.appendChild(para3);
-
-  const contactImage3 = document.createElement("img");
-  contactImage3.classList.add("menu-img");
-  contactImage3.src = "../src/assests/phone-img.png";
-  contactImage3.alt = "Image of Menu";
-  contentDiv.appendChild(contactImage3);
+  addImageElement(contentDiv, contentInfo.srcLink1);
+  addParaElement(contentDiv, contentInfo.paraText1);
+  addImageElement(contentDiv, contentInfo.srcLink2);
+  addParaElement(contentDiv, contentInfo.paraText2);
+  addImageElement(contentDiv, contentInfo.srcLink3);
+  addParaElement(contentDiv, contentInfo.paraText3);
+  addImageElement(contentDiv, contentInfo.srcLink4);
 }
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,28 @@
+const menuInfo = {
+  srcLink1: "../src/assests/menu-img.jpg",
+  srcLink2: "../src/assests/pizza-img.jpg",
+  srcLink3: "../src/assests/burger-img.jpg",
+  srcLink4: "../src/assests/pasta-img.jpg",
+  paraText1: "1. Pizza",
+  paraText2: "2. Burger",
+  paraText3: "3. Pasta",
+};
+
+function addImageElement(contentDiv, srcLink) {
+  const menuImage = document.createElement("img");
+  menuImage.classList.add("menu-img");
+  menuImage.src = srcLink;
+  menuImage.alt = "Image of Menu";
+  contentDiv.appendChild(menuImage);
+}
+
+function addParaElement(contentDiv, paraText) {
+  const para = document.createElement("p");
+  para.classList.add("intro");
+  para.textContent = paraText;
+  contentDiv.appendChild(para);
+}
+
 export function loadMenuPage() {
   const contentDiv = document.querySelector("#content");
   contentDiv.replaceChildren();
@@ -5,43 +30,11 @@ export function loadMenuPage() {
   heading.textContent = "Menu :)";
   heading.classList.add("landing-title");
   contentDiv.appendChild(heading);
-
-  const topImage = document.createElement("img");
-  topImage.classList.add("menu-img");
-  topImage.src = "../src/assests/menu-img.jpg";
-  topImage.alt = "Image of Menu";
-  contentDiv.appendChild(topImage);
-
-  const para1 = document.createElement("p");
-  para1.classList.add("intro");
-  para1.textContent = "1. Pizza";
-  contentDiv.appendChild(para1);
-
-  const menuImage1 = document.createElement("img");
-  menuImage1.classList.add("menu-img");
-  menuImage1.src = "../src/assests/pizza-img.jpg";
-  menuImage1.alt = "Image of Menu";
-  contentDiv.appendChild(menuImage1);
-
-  const para2 = document.createElement("p");
-  para2.classList.add("intro");
-  para2.textContent = "2. Burger";
-  contentDiv.appendChild(para2);
-
-  const menuImage2 = document.createElement("img");
-  menuImage2.classList.add("menu-img");
-  menuImage2.src = "../src/assests/burger-img.jpg";
-  menuImage2.alt = "Image of Menu";
-  contentDiv.appendChild(menuImage2);
-
-  const para3 = document.createElement("p");
-  para3.classList.add("intro");
-  para3.textContent = "3. Pasta";
-  contentDiv.appendChild(para3);
-
-  const menuImage3 = document.createElement("img");
-  menuImage3.classList.add("menu-img");
-  menuImage3.src = "../src/assests/pasta-img.jpg";
-  menuImage3.alt = "Image of Menu";
-  contentDiv.appendChild(menuImage3);
+  addImageElement(contentDiv, menuInfo.srcLink1);
+  addParaElement(contentDiv, menuInfo.paraText1);
+  addImageElement(contentDiv,menuInfo.srcLink2);
+  addParaElement(contentDiv, menuInfo.paraText2);
+  addImageElement(contentDiv, menuInfo.srcLink3);
+  addParaElement(contentDiv, menuInfo.paraText3);
+  addImageElement(contentDiv, menuInfo.srcLink4);
 }
